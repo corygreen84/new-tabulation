69f80eea73fbe3a6a43ede912d66863f59cfdd56
corygreen84 <49203848+corygreen84@users.noreply.github.com>
2023-12-06 20:31:54 -0800

    added in Chord component

---
+++ tabulatron-v2/src/components/Chord.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-container class="pa-0">
+            <v-text-field v-if="index === numberOfStrings + 1" outlined readonly hide-details="" class="centered-input small-text-size rotated ml-0" v-model="chordName"></v-text-field>
+            <v-text-field v-else class="centered-input small-text-size rotated ml-0"></v-text-field>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                chordName: '',
+                tuning: [],
+                numberOfStrings: 6
+            }
+        },
+        props: {    
+        tabColumn: {
+            default: 0
+        },
+    },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- tabulatron-v2/src/views/Home.vue
+++ tabulatron-v2/src/views/Home.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>Hey there</div>
+  <div>
+    <v-col cols="auto">
+      <v-row v-for="(i, index) in 6" :key="index" style="width: 60px; height: 60px;">
+        <Chord numberOfStrings="6"/>
+      </v-row>
+    </v-col>
+  </div>
 </template>
 
 <script setup>
+  import Chord from "./../components/Chord.vue"
 </script>
